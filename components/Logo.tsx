caca303f6025b05b2a7e532e510309551bab820b
Marvin Morelo <haunditing@gmail.com>
2026-02-14 11:12:13 -0500

    Fix: Adjusting SEO

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,23 +2,15 @@ import React from "react";
 
 interface LogoProps {
   className?: string;
-  withText?: boolean;
 }
 
-const Logo: React.FC<LogoProps> = ({
-  className = "h-12 w-12",
-  withText = true,
-}) => {
+const Logo: React.FC<LogoProps> = ({ className = "w-12 h-12" }) => {
   return (
-    <div className="flex flex-col items-center justify-center gap-2">
-      <a href="/" className="group flex flex-col items-center">
-        <img
-          src="/android-chrome-512x512.png"
-          alt="MensajeMágico Icono"
-          className={`object-contain transition-transform duration-300 group-hover:rotate-12 ${className}`}
-        />
-      </a>
-    </div>
+    <img
+      src="/android-chrome-512x512.png"
+      alt="MensajeMágico Logo"
+      className={`object-contain transition-transform duration-300 ${className}`}
+    />
   );
 };
 
