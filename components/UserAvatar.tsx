86e8695eb322fdfa2c08b2d1209a4fc5f5908383
Marvin Morelo <haunditing@gmail.com>
2026-02-20 17:11:34 -0500

    Fix: Update svg siluet for neutral gender

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -85,9 +85,14 @@ const UserAvatar: React.FC<UserAvatarProps> = ({ user, className = "", showStatu
           <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
         </svg>
       ) : (
-        <span className="text-white font-bold text-[40%]">
-          {(user.name ? user.name[0] : user.email[0]).toUpperCase()}
-        </span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-[60%] h-[60%] text-white opacity-90"
+        >
+          <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+        </svg>
       )}
     </div>
   );
